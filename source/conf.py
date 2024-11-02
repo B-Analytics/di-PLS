848f9843496a8ba71de4b18afbce0a2c7fe3c93d
eraemien <eraemien@gmail.com>
2024-11-03 00:33:46 +0100

    updated doc files

--- source/conf.py
+++ source/conf.py
@@ -5,42 +5,28 @@
 
 import os
 import sys
-
-# Add the root directory to the sys.path
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'diPLSlib Documentation'
+project = 'diPLSlib'
 copyright = '2024, Ramin Nikzad-Langerodi'
 author = 'Ramin Nikzad-Langerodi'
-release = '2.1.0'
+release = '2.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'numpydoc',
-    'sphinx_rtd_theme'
-    # 'sphinx.ext.napoleon'
-]
-
-autosummary_generate = True
+    'numpydoc']
+    #'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'en'
 
-autodoc_default_options = {
-    'member-order': 'bysource',
-    'undoc-members': True,
-    'show-inheritance': True,
-    'special-members': '__init__'
-}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
---
+++ source/diPLSlib.rst
@@ -0,0 +1,37 @@
+diPLSlib package
+================
+
+Subpackages
+-----------
+
+.. toctree::
+   :maxdepth: 4
+
+   diPLSlib.utils
+
+Submodules
+----------
+
+diPLSlib.functions module
+-------------------------
+
+.. automodule:: diPLSlib.functions
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+diPLSlib.models module
+----------------------
+
+.. automodule:: diPLSlib.models
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: diPLSlib
+   :members:
+   :undoc-members:
+   :show-inheritance:
